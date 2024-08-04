18cf0a0168be5b0a70c64d59a2398776711781ff
Achaaasa <josephachaasa@gmail.com>
2024-08-04 16:38:10 +0000

    added the routes for the document

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import authRouter from "./auth.route";
 import userRouter from "./userRoute";
+import documentRouter from "./documentRoute";
 
 const appRouter = Router();
-appRouter.use("/authentication", authRouter)
-appRouter.use("/users", userRouter)
-
+appRouter.use("/authentication", authRouter);
+appRouter.use("/users", userRouter);
+appRouter.use("/documents", documentRouter);
 export default appRouter;
